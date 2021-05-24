0d5ecc474311341073ddd88a66fd4f7ab48708dd
AkiraSuzukiJP <ec2-user@ip-172-31-18-239.ap-northeast-1.compute.internal>
2021-05-24 02:03:22 +0000

    menu表示（仮）

--- resources/js/components/AircraftsComponent.vue
+++ resources/js/components/AircraftsComponent.vue
@@ -1,3 +1,65 @@
 <template>
-	<h1>AircraftsComponent</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-start mb-3">
+                        <div class="mr-auto">
+                            <span class="span-header">機体一覧</span>
+                        </div>
+                        <div class="align-self-center">
+                            <button type="button" class="btn btn-dark" @click="onBack">戻る</button>
+                        </div>
+                    </div>
+                    
+                    // ここにデータを表示
+                    <h3>登録機体</h3>
+                    <p>-results-----------------</p>
+                    {{results}}
+                    <p>-------------------------</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
+
+<script>
+export default {
+    props: {
+        //
+    },
+    data () {
+        return {
+            //
+            results: null,
+        }
+    },
+    mounted () {
+        //
+    },
+    watch: {
+        //
+    },
+    computed: {
+        //
+    },
+    methods: {
+        async getInit() {
+            // routes/api.phpにルーティングを設定する
+            // Route::get('lesson/selector', 'API\LessonController@index');
+            // ↑これがControllerとの紐づけ定義
+            const {data} = await axios.get('/api/aircrafts')
+            this.results = data
+        },
+        onBack() {
+            this.$router.push({ name: 'menu' })
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "resources/sass/variables";
+</style>
