c72c883ea5f7dec819371c9c7ba0815df3b75941
AkiraSuzukiJP <ec2-user@ip-172-31-18-239.ap-northeast-1.compute.internal>
2021-05-24 00:24:09 +0000

    menu表示（仮）

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,13 +5,7 @@ import Menu from './components/MenuComponent.vue'
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/',
-      name: 'menu',
-      component: Menu
-    },
-    { path: '/',
-      name: 'home',
-      component: Home
-    },
+    { path: '/', name: 'menu', component: Menu },
+    { path: '/', name: 'home', component: Home },
   ]
 });
